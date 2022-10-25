64309dbbd66410e89682cad7438dd3505af52dc5
Kathyellen Souza <87395970+souzakathyellen@users.noreply.github.com>
2022-10-25 16:31:59 -0300

    Add files via upload

---
+++ ProgrmaMedia/ProgrmaMedia/src/ProgrmaMedia.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class ProgrmaMedia {
+    public static void main(String[] args) throws Exception {
+        Scanner teclado = new Scanner(System.in);
+        
+    }
+}
